022a4f7ba8649b47b402ff6a2630904d791d6d19
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-08 10:29:29 +0300

    style(BoardSelector): done of building the jsx structure & styles

--- src/components/Header/HeaderButtons/BoardSelector/BoardSelector.jsx
+++ src/components/Header/HeaderButtons/BoardSelector/BoardSelector.jsx
@@ -1,7 +1,13 @@
 import s from "./BoardSelector.module.scss";
 
 const BoardSelector = () => {
-  return <div className={s.boardSelector}></div>;
+  return (
+    <div className={s.boardSelector}>
+      <button data-size="3">3x3</button>
+      <button data-size="4">4x4</button>
+      <button data-size="5">5x5</button>
+    </div>
+  );
 };
 
 export default BoardSelector;
