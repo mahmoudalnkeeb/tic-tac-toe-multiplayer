839ac3eae59e49f2d842f20c6cb46b5bb1af9083
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-08 16:46:22 +0300

    feat(XOSquare): make the squares size smaller on 4x4/5x5 mode

--- src/components/XOGame/XOBoard/XOSquare/XOSquare.jsx
+++ src/components/XOGame/XOBoard/XOSquare/XOSquare.jsx
@@ -1,10 +1,14 @@
 import { SYMBOL_O, SYMBOL_X } from "@/data/constants";
+import { useXOStore } from "@/stores/xo.store";
 import s from "./XOSquare.module.scss";
 
 const XOSquare = ({ value, disabled, onClick }) => {
+  const boardSize = useXOStore((s) => s.boardSize);
+  const x4Class = boardSize === 4 ? s.x4 : "";
+  const x5Class = boardSize === 5 ? s.x5 : "";
   const playerXClass = value === SYMBOL_X ? s.playerX : "";
   const playerOClass = value === SYMBOL_O ? s.playerO : "";
-  const classes = `${s.square} ${playerXClass} ${playerOClass}`;
+  const classes = `${s.square} ${playerXClass} ${playerOClass} ${x4Class} ${x5Class}`;
 
   return (
     <button
