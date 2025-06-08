92543e738b94da291bcdf0b1ab7c7a19f2e8cc01
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-08 10:54:59 +0300

    feat(ResetButton): handle click to reset game (start a new game)

--- src/components/Header/HeaderButtons/ResetButton/ResetButton.jsx
+++ src/components/Header/HeaderButtons/ResetButton/ResetButton.jsx
@@ -1,8 +1,13 @@
+"use client";
+
+import { useXOStore } from "@/stores/xo.store";
 import s from "./ResetButton.module.scss";
 
 const ResetButton = () => {
+  const resetGame = useXOStore((s) => s.resetGame);
+
   return (
-    <button type="button" className={s.resetButton}>
+    <button type="button" className={s.resetButton} onClick={resetGame}>
       Reset
     </button>
   );
