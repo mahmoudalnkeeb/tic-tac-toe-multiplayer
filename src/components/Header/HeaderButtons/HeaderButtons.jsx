5e090e22b0ebabc1d6f3cd7357f6b2815c02ad44
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-08 10:52:24 +0300

    init: ResetButton component in HeaderButtons

--- src/components/Header/HeaderButtons/HeaderButtons.jsx
+++ src/components/Header/HeaderButtons/HeaderButtons.jsx
@@ -1,12 +1,14 @@
 import AboutButton from "./AboutButton/AboutButton";
 import BoardSelector from "./BoardSelector/BoardSelector";
 import s from "./HeaderButtons.module.scss";
+import ResetButton from "./ResetButton/ResetButton";
 
 const HeaderButtons = () => {
   return (
     <div className={s.headerButtons}>
       <BoardSelector />
       <AboutButton />
+      <ResetButton />
     </div>
   );
 };
---
+++ src/components/Header/HeaderButtons/ResetButton/ResetButton.jsx
@@ -0,0 +1,11 @@
+import s from "./ResetButton.module.scss";
+
+const ResetButton = () => {
+  return (
+    <button type="button" className={s.resetButton}>
+      Reset
+    </button>
+  );
+};
+
+export default ResetButton;
