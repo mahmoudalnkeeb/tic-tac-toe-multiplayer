fcfffd8d9c81af0a11dc82d0817e0671cd26cfb9
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-08 10:57:02 +0300

    remove: GameMessages component

--- src/components/XOGame/XOGame.jsx
+++ src/components/XOGame/XOGame.jsx
@@ -1,15 +1,11 @@
 "use client";
 
-import GameMessages from "./GameMessages/GameMessages";
 import XOBoard from "./XOBoard/XOBoard";
 import s from "./XOGame.module.scss";
 
 const XOGame = () => {
   return (
     <section className={s.game}>
-      <div className={s.controls}>
-        <GameMessages />
-      </div>
       <XOBoard />
     </section>
   );
