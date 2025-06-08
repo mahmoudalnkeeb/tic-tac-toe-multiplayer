68f30c57ad595a697cc04c4083cce23c5adeb97d
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-08 10:22:08 +0300

    init: HeaderButtons component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import s from "./Header.module.scss";
+import HeaderButtons from "./HeaderButtons/HeaderButtons";
 import Logo from "./Logo/Logo";
 
 const Header = () => {
   return (
     <header className={s.header}>
       <Logo />
+      <HeaderButtons />
     </header>
   );
 };
---
+++ src/components/Header/HeaderButtons/HeaderButtons.jsx
@@ -0,0 +1,9 @@
+import s from './HeaderButtons.module.scss'
+
+const HeaderButtons = () => {
+  return (
+    <div>HeaderButtons</div>
+  )
+}
+
+export default HeaderButtons
