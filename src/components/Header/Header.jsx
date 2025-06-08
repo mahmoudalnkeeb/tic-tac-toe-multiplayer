b5155361414f1f9b5ad09ba77e5dd27bfa191a3d
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-08 10:08:25 +0300

    init: Logo component in the Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
 import s from "./Header.module.scss";
+import Logo from "./Logo/Logo";
 
 const Header = () => {
-  return <header className={s.header}></header>;
+  return (
+    <header className={s.header}>
+      <Logo />
+    </header>
+  );
 };
 
 export default Header;
---
+++ src/components/Header/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import s from './Logo.module.scss'
+
+const Logo = () => {
+  return (
+    <div className={s.logo}></div>
+  )
+}
+
+export default Logo
