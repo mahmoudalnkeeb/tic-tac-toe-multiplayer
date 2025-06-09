28524efc6a463282afea49ff384b4267cea1037f
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-09 14:38:29 +0300

    style(PowerUps): add 3 power-ups buttons with their styles

--- src/components/XOGame/PowerUps/PowerUps.jsx
+++ src/components/XOGame/PowerUps/PowerUps.jsx
@@ -1,7 +1,27 @@
 import s from "./PowerUps.module.scss";
 
 const PowerUps = () => {
-  return <div className={s.powerUps}></div>;
+  return (
+    <div className={s.powerUps}>
+      <div className={`${s.powerUp} ${true ? s.disabled : ""}`}>
+        <span className={s.icon}>❄️</span>
+        <span className={s.powerName}>Freeze</span>
+        <span className={s.coolDown}>6</span>
+      </div>
+
+      <div className={`${s.powerUp} ${false ? s.disabled : ""}`}>
+        <span className={s.icon}>💣</span>
+        <span className={s.powerName}>Bomb</span>
+        <span className={s.coolDown}>6</span>
+      </div>
+
+      <div className={`${s.powerUp} ${false ? s.disabled : ""}`}>
+        <span className={s.icon}>🔄</span>
+        <span className={s.powerName}>Swap</span>
+        <span className={s.coolDown}>6</span>
+      </div>
+    </div>
+  );
 };
 
 export default PowerUps;
