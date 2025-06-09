d5573195eb677f2f4841e62c3833c9a8fdd0dc7a
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-09 08:35:06 +0300

    update(GameStats): write jsx structure & styles

--- src/components/XOGame/GameStats/GameStats.jsx
+++ src/components/XOGame/GameStats/GameStats.jsx
@@ -1,7 +1,24 @@
 import s from "./GameStats.module.scss";
 
 const GameStats = () => {
-  return <div className={s.gameStats}></div>;
+  return (
+    <div className={s.gameStats}>
+      <div className={s.stat} id="p1">
+        <span className={s.value}>0</span>
+        <span className={s.label}>P1 Wins</span>
+      </div>
+
+      <div className={s.stat} id="draws">
+        <span className={s.value}>0</span>
+        <span className={s.label}>Draws</span>
+      </div>
+
+      <div className={s.stat} id="p2">
+        <span className={s.value}>0</span>
+        <span className={s.label}>P2 Wins</span>
+      </div>
+    </div>
+  );
 };
 
 export default GameStats;
--- src/components/XOGame/XOGame.jsx
+++ src/components/XOGame/XOGame.jsx
@@ -1,11 +1,13 @@
 "use client";
 
+import GameStats from "./GameStats/GameStats";
 import XOBoard from "./XOBoard/XOBoard";
 import s from "./XOGame.module.scss";
 
 const XOGame = () => {
   return (
     <section className={s.game}>
+      <GameStats />
       <XOBoard />
     </section>
   );
