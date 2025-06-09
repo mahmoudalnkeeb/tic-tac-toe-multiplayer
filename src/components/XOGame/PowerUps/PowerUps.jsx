9977355a8db0ff5ae3c190caec5c9e817ad30391
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-09 14:49:28 +0300

    feat(PowerUps): hide power-ups when board size is 3x3

--- src/components/XOGame/PowerUps/PowerUps.jsx
+++ src/components/XOGame/PowerUps/PowerUps.jsx
@@ -1,10 +1,15 @@
+"use client";
+
+import { useXOStore } from "@/stores/xo.store";
 import s from "./PowerUps.module.scss";
 
 const PowerUps = ({ player }) => {
+  const { boardSize } = useXOStore((s) => s);
   const p1Class = player === "p1" ? s.p1 : "";
+  const hiddenClass = boardSize === 3 ? s.hidden : "";
 
   return (
-    <div className={`${s.powerUps} ${p1Class}`}>
+    <div className={`${s.powerUps} ${p1Class} ${hiddenClass}`}>
       <div className={`${s.powerUp} ${false ? s.disabled : ""}`}>
         <span className={s.icon}>❄️</span>
         <span className={s.powerName}>Freeze</span>
