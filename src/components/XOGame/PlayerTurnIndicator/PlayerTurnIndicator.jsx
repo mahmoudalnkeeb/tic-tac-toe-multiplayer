e6a742c4bfe9507a5c82b7967e120a5065715d7f
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-09 10:16:25 +0300

    feat(PlayerTurnIndicator): build the component to indicate player turn with neat styles

--- src/components/XOGame/PlayerTurnIndicator/PlayerTurnIndicator.jsx
+++ src/components/XOGame/PlayerTurnIndicator/PlayerTurnIndicator.jsx
@@ -1,7 +1,27 @@
+"use client";
+
+import { SYMBOL_O, SYMBOL_X } from "@/data/constants";
+import { useXOStore } from "@/stores/xo.store";
 import s from "./PlayerTurnIndicator.module.scss";
 
 const PlayerTurnIndicator = () => {
-  return <div className={s.indicator}></div>;
+  const playerTurn = useXOStore((s) => s.playerTurn);
+  const p1ActiveClass = playerTurn === SYMBOL_O ? s.active : "";
+  const p2ActiveClass = playerTurn === SYMBOL_X ? s.active : "";
+
+  return (
+    <div className={s.indicator}>
+      <div className={`${s.player} ${s.p1} ${p1ActiveClass}`}>
+        <span className={s.symbol}>○</span>
+        <span className={s.label}>P1</span>
+      </div>
+
+      <div className={`${s.player} ${s.p2} ${p2ActiveClass}`}>
+        <span className={s.symbol}>✕</span>
+        <span className={s.label}>P2</span>
+      </div>
+    </div>
+  );
 };
 
 export default PlayerTurnIndicator;
