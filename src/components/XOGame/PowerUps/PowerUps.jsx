f1d606ba5e5f40372199d7250ac34126750d2500
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-11 17:37:29 +0300

    style: support responsiveness to all devices (desktop/mobile)

--- src/components/XOGame/PowerUps/PowerUps.jsx
+++ src/components/XOGame/PowerUps/PowerUps.jsx
@@ -9,16 +9,22 @@ import s from "./PowerUps.module.scss";
 const PowerUps = ({ player }) => {
   const { boardSize, powerUps, playerTurn } = useXOStore((s) => s);
   const playerPowerUps = Object.entries(powerUps[player]);
-  const hiddenClass = boardSize === 3 ? s.hidden : "";
+  const isPlayer1 = playerTurn !== SYMBOL_O && player === "player1";
+  const isPlayer2 = playerTurn !== SYMBOL_X && player === "player2";
+
+  const classes = [
+    s.powerUps,
+    boardSize === 3 ? s.hidden : "",
+    player === "player1" ? s.player1 : "",
+    player === "player2" ? s.player2 : "",
+    !(isPlayer1 || isPlayer2) ? s.active : "",
+  ].join(" ");
 
   return (
-    <div className={`${s.powerUps} ${hiddenClass}`}>
+    <div className={classes}>
       {POWER_UPS_BUTTONS.map((buttonData, index) => {
         const { available, coolDown } = playerPowerUps[index][1];
-        const disable =
-          !available ||
-          (playerTurn !== SYMBOL_O && player === "player1") ||
-          (playerTurn !== SYMBOL_X && player === "player2");
+        const disable = !available || isPlayer1 || isPlayer2;
 
         return (
           <PowerUpButton
