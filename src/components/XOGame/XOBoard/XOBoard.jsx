c751e98317d9fbdc63a0f6fd912cff763abfa58c
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-12 18:44:50 +0300

    feat(XOBoard): change boarder color of the board based on player turn color

--- src/components/XOGame/XOBoard/XOBoard.jsx
+++ src/components/XOGame/XOBoard/XOBoard.jsx
@@ -1,13 +1,15 @@
+import { SYMBOL_X } from "@/data/constants";
 import { useXOStore } from "@/stores/xo.store/xo.store";
 import BoardRow from "./BoardRow/BoardRow";
 import WinnerPopUp from "./WinnerPopUp/WinnerPopUp";
 import s from "./XOBoard.module.scss";
 
 const XOBoard = () => {
-  const { board } = useXOStore((state) => state);
+  const { board, playerTurn } = useXOStore((state) => state);
+  const playerTurnClass = playerTurn === SYMBOL_X ? s.xTurn : s.oTurn;
 
   return (
-    <div className={s.board}>
+    <div className={`${s.board} ${playerTurnClass}`}>
       {board.map((row, rowIndex) => (
         <BoardRow key={rowIndex} row={row} rowIndex={rowIndex} />
       ))}
