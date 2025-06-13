48ad500ddabefaa7ad3c372c6621cfca3a877e59
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-13 12:34:39 +0300

    remove: unused page.module.scss file

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
 "use client";
 
 import XOGame from "@/components/XOGame/XOGame";
-import s from "./page.module.scss";
 
 export default function Home() {
   return (
-    <main className={s.home}>
+    <main>
       <XOGame />
     </main>
   );
