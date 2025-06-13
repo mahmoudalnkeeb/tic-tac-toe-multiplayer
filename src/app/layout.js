7eb0c59b1827e1ba68dd5493424d1aadff31b7e3
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-13 12:49:53 +0300

    refactor(layout): extract metadata to separate file for clarity

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,8 @@
 import Header from "@/components/Header/Header";
+import { METADATA } from "@/data/metadata";
 import "../styles/globals.scss";
 
-export const metadata = {
-  title: "Advanced Tic Tic Toe",
-  description:
-    "A fun and strategic Tic Tac Toe game with board sizes up to 5x5 and exciting power-ups like Freeze, Bomb, and Swap. Learn how to play, challenge a friend, and enjoy a new level of competition!",
-  keywords: [
-    "advanced tic tac toe",
-    "tic tac toe with power ups",
-    "play tic tac toe online",
-    "5x5 tic tac toe",
-    "4x4 tic tac toe",
-    "custom board tic tac toe",
-    "tic tac toe strategy game",
-    "multiplayer tic tac toe",
-    "online tic tac toe game",
-    "interactive tic tac toe",
-    "freeze bomb swap tic tac toe",
-    "modern tic tac toe game",
-    "fun tic tac toe variations",
-    "how to play tic tac toe with powers",
-    "play tic tac toe with friends",
-  ],
-};
+export const metadata = METADATA;
 
 export default function RootLayout({ children }) {
   return (
