64c5fafb8f2c8337cf6624f727dc00fbe219f5ce
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-13 18:01:54 +0300

    Revert "feat(XOBoard): highlight board border in yellow when game ends in a draw"
    This reverts commit 46ab44a5d53d02fda09ed83d6de9406a223898fc.

--- src/components/XOGame/XOBoard/XOBoard.jsx
+++ src/components/XOGame/XOBoard/XOBoard.jsx
@@ -5,12 +5,11 @@ import WinnerPopUp from "./WinnerPopUp/WinnerPopUp";
 import s from "./XOBoard.module.scss";
 
 const XOBoard = () => {
-  const { board, playerTurn, winner } = useXOStore((state) => state);
+  const { board, playerTurn } = useXOStore((state) => state);
   const playerTurnClass = playerTurn === SYMBOL_X ? s.xTurn : s.oTurn;
-  const drawClass = winner === "Draw!" ? s.draw : "";
 
   return (
-    <div className={`${s.board} ${playerTurnClass} ${drawClass}`}>
+    <div className={`${s.board} ${playerTurnClass}`}>
       {board.map((row, rowIndex) => (
         <BoardRow key={rowIndex} row={row} rowIndex={rowIndex} />
       ))}
