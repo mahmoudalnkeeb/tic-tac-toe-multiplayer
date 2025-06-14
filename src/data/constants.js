aaaee257f29808730210b0459ad7b304d80d0415
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-14 19:56:11 +0300

    update(constants): decrease BOMB_DELETION_DELAY_MS from 1000 to 800

--- src/data/constants.js
+++ src/data/constants.js
@@ -4,5 +4,5 @@ export const SYMBOL_X = "✕";
 export const FIRST_PLAYER = SYMBOL_X;
 export const INITIAL_BOARD_SIZE = 4;
 export const SWAP_SYMBOL_DELAY_MS = 800;
-export const BOMB_DELETION_DELAY_MS = 1000;
+export const BOMB_DELETION_DELAY_MS = 800;
 export const WINNER_POPUP_DURATION_MS = 2000;
