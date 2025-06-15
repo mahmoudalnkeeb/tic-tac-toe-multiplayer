4854dbba5a1810bbca18cb171fae8f6bc0c88a5b
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-16 01:05:55 +0300

    update(metadata): fix title typo

--- src/data/metadata.js
+++ src/data/metadata.js
@@ -1,7 +1,7 @@
 export const DOMAIN = "https://tictactoe-4x4.netlify.app";
 
 export const METADATA = {
-  title: "Advanced Tic Tic Toe",
+  title: "Advanced Tic Tac Toe",
   description:
     "A fun and strategic Tic Tac Toe game with board sizes up to 5x5 and exciting power-ups like Freeze, Bomb, and Swap. Learn how to play, challenge a friend, and enjoy a new level of competition!",
   keywords: [
@@ -13,7 +13,7 @@ export const METADATA = {
     "custom board tic tac toe",
     "tic tac toe strategy game",
     "multiplayer tic tac toe",
-    "online tic tac toe game",
+    "online tic tac toe game", 
     "interactive tic tac toe",
     "freeze bomb swap tic tac toe",
     "modern tic tac toe game",
