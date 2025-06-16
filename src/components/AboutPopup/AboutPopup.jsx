0a6269958604a8e43786dfbcf32ad509223d3a6e
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-17 01:25:08 +0300

    fix(AboutPopup): ensure only outside overlay clicks close the popup

--- src/components/AboutPopup/AboutPopup.jsx
+++ src/components/AboutPopup/AboutPopup.jsx
@@ -1,19 +1,29 @@
 "use client";
 
 import { useGlobalStore } from "@/stores/xo.store/global.store";
+import { useRef } from "react";
 import AboutHeader from "./AboutHeader/AboutHeader";
 import s from "./AboutPopup.module.scss";
 
 const AboutPopup = () => {
   const { isAboutModelActive, toggleAboutModel } = useGlobalStore((s) => s);
+  const aboutArticleRef = useRef(null);
   const showClass = isAboutModelActive ? s.show : "";
+
+  function handleOverlayClick(event) {
+    const popupElement = aboutArticleRef.current;
+    const clickedInsidePopup = popupElement.contains(event.target);
+
+    if (clickedInsidePopup) return;
+    toggleAboutModel(false);
+  }
 
   return (
     <div
       className={`${s.aboutOverlay} ${showClass}`}
-      onClick={() => toggleAboutModel(false)}
+      onClick={handleOverlayClick}
     >
-      <article className={s.aboutArticle}>
+      <article className={s.aboutArticle} ref={aboutArticleRef}>
         <AboutHeader />
       </article>
     </div>
