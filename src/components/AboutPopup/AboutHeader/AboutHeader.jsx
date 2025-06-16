f49fee7d012285a377e52d7166798dc302c90ea0
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-17 01:16:16 +0300

    style: build AboutHeader component with its styles

--- src/components/AboutPopup/AboutHeader/AboutHeader.jsx
+++ src/components/AboutPopup/AboutHeader/AboutHeader.jsx
@@ -1,9 +1,26 @@
-import s from './AboutHeader.module.scss'
+"use client";
+
+import { SYMBOL_X } from "@/data/constants";
+import { useGlobalStore } from "@/stores/xo.store/global.store";
+import s from "./AboutHeader.module.scss";
 
 const AboutHeader = () => {
-  return (
-    <header className={s.header}></header>
-  )
-}
+  const toggleAboutModel = useGlobalStore((s) => s.toggleAboutModel);
 
-export default AboutHeader
+  return (
+    <header className={s.header}>
+      <h2 className={s.title}>About Tic Tac Toe</h2>
+
+      <button
+        type="button"
+        className={s.closeBtn}
+        onClick={() => toggleAboutModel(false)}
+        aria-label="Close About Popup"
+      >
+        {SYMBOL_X}
+      </button>
+    </header>
+  );
+};
+
+export default AboutHeader;
