2c30139b4f478a86a72af8501b2cac2216d7d60f
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-16 16:16:31 +0300

    feat(Header): make Logo clickable by wrapping it with Link to homepage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import s from "./Header.module.scss";
 import HeaderButtons from "./HeaderButtons/HeaderButtons";
 import Logo from "./Logo/Logo";
@@ -5,7 +6,9 @@ import Logo from "./Logo/Logo";
 const Header = () => {
   return (
     <header className={s.header}>
-      <Logo />
+      <Link href="/">
+        <Logo />
+      </Link>
       <HeaderButtons />
     </header>
   );
