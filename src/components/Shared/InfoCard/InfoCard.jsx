a9fc078df50055c18244a59383855a7a6f04016e
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-17 01:37:44 +0300

    style(InfoCard): build the component with its styles

--- src/components/Shared/InfoCard/InfoCard.jsx
+++ src/components/Shared/InfoCard/InfoCard.jsx
@@ -1,7 +1,12 @@
 import s from "./InfoCard.module.scss";
 
-const InfoCard = () => {
-  return <section className={s.infoCard}></section>;
+const InfoCard = ({ title, children }) => {
+  return (
+    <section className={s.infoCard}>
+      <h3 className={s.title}>{title}</h3>
+      {children}
+    </section>
+  );
 };
 
 export default InfoCard;
