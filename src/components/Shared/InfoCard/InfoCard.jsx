ee203de2cfa5efa66f917adf7457d739cb18570d
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-17 18:07:06 +0300

    feat(InfoCard): style nested InfoCard differently

--- src/components/Shared/InfoCard/InfoCard.jsx
+++ src/components/Shared/InfoCard/InfoCard.jsx
@@ -1,9 +1,10 @@
 import s from "./InfoCard.module.scss";
 
-const InfoCard = ({ title, children }) => {
+const InfoCard = ({ title, isNested, children }) => {
   return (
-    <section className={s.infoCard}>
-      <h3 className={s.title}>{title}</h3>
+    <section className={`${s.infoCard} ${isNested ? s.nestedCard : ""}`}>
+      {!isNested && <h3 className={s.title}>{title}</h3>}
+      {isNested && <h4 className={s.title}>{title}</h4>}
       {children}
     </section>
   );
