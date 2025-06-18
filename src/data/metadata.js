88c90e7f24353e53936ce46001f893f00d8de36a
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-19 02:06:40 +0300

    feat(metadata): add PWA metadata properties

--- src/data/metadata.js
+++ src/data/metadata.js
@@ -1,6 +1,36 @@
 export const DOMAIN = "https://tictactoe-4x4.netlify.app";
 
-export const METADATA = {
+const PWA_METADATA = {
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "Moamal Alaa",
+  },
+  icons: {
+    icon: [
+      {
+        url: "/PWA/icons/maskable-icon.webp",
+        sizes: "192x192",
+        type: "image/webp",
+      },
+      {
+        url: "/PWA/icons/maskable-icon.webp",
+        sizes: "512x512",
+        type: "image/webp",
+      },
+    ],
+    apple: [
+      {
+        url: "/PWA/icons/maskable-icon.webp",
+        sizes: "180x180",
+        type: "image/webp",
+      },
+    ],
+  },
+};
+
+export const GLOBAL_METADATA = {
   title: "Advanced Tic Tac Toe",
   description:
     "A fun and strategic Tic Tac Toe game with board sizes up to 5x5 and exciting power-ups like Freeze, Bomb, and Swap. Learn how to play, challenge a friend, and enjoy a new level of competition!",
@@ -13,7 +43,7 @@ export const METADATA = {
     "custom board tic tac toe",
     "tic tac toe strategy game",
     "multiplayer tic tac toe",
-    "online tic tac toe game", 
+    "online tic tac toe game",
     "interactive tic tac toe",
     "freeze bomb swap tic tac toe",
     "modern tic tac toe game",
@@ -25,3 +55,5 @@ export const METADATA = {
   publisher: "Moamal Alaa Kareem",
   creator: "Moamal Alaa Kareem",
 };
+
+export const METADATA = { ...GLOBAL_METADATA, ...PWA_METADATA };
